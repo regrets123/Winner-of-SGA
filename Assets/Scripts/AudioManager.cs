7a87a3f13efbd0eee6b4610fd951f9acc035156b
KazzTruul <kazztruul@hotmail.com>
2018-02-20 15:22:57 +0100

    PauseManager & AudioManager added

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    [SerializeField]
+    List<AudioSource>[] soundGroups = new List<AudioSource>[2];
+
+    List<AudioSource> environmentalAudio;
+    List<AudioSource> soundFX;
+    AudioSource music;
+
+    public void ChangeVolume(int soundGroup, float volume)
+    {
+        foreach(AudioSource audio in soundGroups[soundGroup])
+        {
+            audio.volume = volume;
+        }
+    }
+
+    public List<AudioSource>[] SoundGroups
+    {
+        get { return this.soundGroups; }
+    }
+
+    public AudioSource Music
+    {
+        get { return this.music; }
+    }
+    
+}
---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject pauseMenu;
+
+    AudioManager aM;
+
+    bool paused = false;
+
+    private void Start()
+    {
+        aM = GetComponent<AudioManager>();
+    }
+
+    public void PauseAndUnpause()
+    {
+        paused = !paused;
+        pauseMenu.SetActive(paused);
+        foreach (List<AudioSource> soundGroup in aM.SoundGroups)
+        {
+            foreach (AudioSource audio in soundGroup)
+            {
+                if (paused)
+                {
+                    audio.Pause();
+                }
+                else
+                {
+                    audio.UnPause();
+                }
+            }
+        }
+        if (paused)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+}
