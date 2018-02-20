59dcf413f96f0b1cb9cc574cfc7a21aa04958311
TacoBellDoge <aforandreas@hotmail.com>
2018-02-20 16:17:10 +0100

    Player Controls/Camera
    Player character movement script och camera rotation till player character.

---
+++ Assets/Scripts/Player Character/PlayerCamera.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+    [SerializeField]
+    float sensitivityX = 5.0f;
+
+    [SerializeField]
+    float sensitivityY = 5.0f;
+
+    public float minimumY = -30.0f;
+    public float maximumY = 30.0f;
+
+    float rotationY = 0.0f;
+    float rotationX = 0.0f;
+
+    float mouseX = 0.0f;
+
+    [SerializeField]
+    GameObject camBase, playerChar;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        CameraMovement();
+    }
+
+    public void CameraMovement()
+    {
+        rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+        camBase.transform.localEulerAngles = new Vector3(-rotationY, 0f, 0);
+        playerChar.transform.localEulerAngles = new Vector3(0f, -rotationX, 0f);
+    }
+}
---
+++ Assets/Scripts/Player Character/PlayerControls.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    CharacterController charController;
+
+    [SerializeField]
+    int health, stamina;
+
+    [SerializeField]
+    float jumpSpeed = 20.0f;
+
+    [SerializeField]
+    float gravity = 1.0f;
+
+    [SerializeField]
+    float moveSpeed = 5.0f;
+
+    public float h;
+    public float v;
+
+    float yVelocity = 0.0f;
+
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    public void PlayerMovement()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(h, 0, v);
+        Vector3 velocity = direction * moveSpeed;
+
+        if (charController.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                yVelocity = jumpSpeed;
+            }
+        }
+        else
+        {
+            yVelocity -= gravity;
+        }
+
+        velocity.y = yVelocity;
+        velocity = transform.TransformDirection(velocity);
+
+        charController.Move(velocity * Time.deltaTime);
+    }
+}
