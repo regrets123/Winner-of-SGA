298a1b70e49290e60424ed4a0bf8c2addaa3c713
TacoBellDoge <aforandreas@hotmail.com>
2018-02-21 16:44:19 +0100

    Removed old cameracontroller, fixed camera collision, fixed new camera controller

---
+++ Assets/Scripts/Player Character/CameraFollow.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public float cameraMoveSpeed = 120.0f;
+    public GameObject cameraFollowObj;
+    Vector3 followPOS;
+    public float clampAngle = 60.0f;
+    public float inputSensitivity = 150.0f;
+    public GameObject cameraObj;
+    public GameObject playerObj;
+    public float camDistanceXToPlayer;
+    public float camDistanceYToPlayer;
+    public float camDistanceZToPlayer;
+    public float mouseX;
+    public float mouseY;
+    public float finalInputX;
+    public float finalInputZ;
+    public float smoothX;
+    public float smoothY;
+    public float rotX = 0.0f;
+    public float rotY = 0.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotX = rot.x;
+        rotY = rot.y;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float inputX = Input.GetAxis("RightStickHorizontal");
+        float inputZ = Input.GetAxis("RightStickVertical");
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        finalInputX = inputX + mouseX;
+        finalInputZ = inputZ + mouseY;
+
+        rotY += finalInputX * inputSensitivity * Time.deltaTime;
+        rotX += finalInputZ * inputSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+
+        transform.rotation = localRotation;
+        //cameraFollowObj.transform.Rotate(cameraFollowObj.transform.rotation.x, transform.rotation.y, cameraFollowObj.transform.rotation.z);
+    }
+
+    void LateUpdate()
+    {
+        CameraUpdater();
+    }
+
+    void CameraUpdater()
+    {
+        Transform target = cameraFollowObj.transform;
+
+        float step = cameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
--- Assets/Scripts/Player Character/PlayerControls.cs
+++ Assets/Scripts/Player Character/PlayerControls.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 /*By Andreas Nilsson*/
 
 
-    //Interface som används av spelaren och alla fiender samt eventuella förstörbara objekt
+//Interface som används av spelaren och alla fiender samt eventuella förstörbara objekt
 public interface IKillable
 {
     void TakeDamage(int damage);
@@ -23,7 +23,7 @@ public class PlayerControls : MonoBehaviour, IKillable
     float gravity = 1.0f;
 
     [SerializeField]
-    int maxHealth;
+    int maxHealth, rotspeed;
 
     [SerializeField]
     float maxStamina;
@@ -31,14 +31,15 @@ public class PlayerControls : MonoBehaviour, IKillable
     [SerializeField]
     float moveSpeed = 5.0f;
 
-    public float h;
-    public float v;
-
     float yVelocity = 0.0f;
 
-    float stamina;
+    float stamina, h, v;
 
     int health;
+
+    private Transform cam;
+
+    private Vector3 camForward;
 
     public void TakeDamage(int damage)
     {
@@ -62,6 +63,7 @@ public class PlayerControls : MonoBehaviour, IKillable
     void Start()
     {
         charController = GetComponent<CharacterController>();
+        cam = FindObjectOfType<Camera>().transform;
     }
 
     private void Update()
@@ -81,18 +83,26 @@ public class PlayerControls : MonoBehaviour, IKillable
             }
         }
         PlayerMovement(sprinting);
+        //MoveMe();
     }
 
-    public void PlayerMovement(bool sprinting)
+    void MoveMe()
     {
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
 
-        Vector3 direction = new Vector3(h, 0, v);
-        Vector3 velocity = direction * moveSpeed;
+        Vector3 movement = new Vector3(h, 0, v);
+        Vector3 velocity = movement * moveSpeed;
+        if (velocity != Vector3.zero)
+        {
+            Quaternion newRot = transform.rotation;
+            newRot.y = cam.transform.rotation.y;
+            transform.rotation = newRot;
+        }
 
         if (charController.isGrounded)
         {
+
             if (Input.GetButtonDown("Jump"))
             {
                 yVelocity = jumpSpeed;
@@ -104,8 +114,73 @@ public class PlayerControls : MonoBehaviour, IKillable
         }
 
         velocity.y = yVelocity;
-        velocity = transform.TransformDirection(velocity);
+        //velocity = transform.TransformDirection(velocity);
 
-        charController.Move(velocity * Time.deltaTime);
+        charController.Move(movement * moveSpeed * Time.deltaTime);
+
+
+        //transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+    }
+
+    public void PlayerMovement(bool sprinting)
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        camForward = Vector3.Scale(cam.forward, new Vector3(1, 0, 1).normalized);
+
+        Vector3 move = v * camForward + h * cam.right;
+
+        if (move.magnitude > 0.0000001f)
+        {
+            move.Normalize();
+            move *= moveSpeed;
+            transform.rotation = Quaternion.LookRotation(move);
+        }
+
+
+        charController.Move(move / 8);
+
+        //Vector3 movement = new Vector3(h, 0, v);
+        //Vector3 velocity = movement * moveSpeed;
+
+        //movement = transform.TransformDirection(movement);
+
+        //Quaternion newRot = transform.rotation;
+        //newRot.y = cam.transform.rotation.y;
+
+        /*
+        Vector3 nextDir = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        if (nextDir != Vector3.zero)
+        {
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, nextDir, Time.deltaTime * rotspeed, 0.0f);
+        }
+
+        charController.Move(nextDir/8);
+        */
+        //transform.Rotate(0f, Input.GetAxis("Horizontal") * rotspeed, 0f);
+
+        //charController.SimpleMove(movement);
+
+        // movement = cam.transform.TransformDirection(movement);
+
+        if (charController.isGrounded)
+        {
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                yVelocity = jumpSpeed;
+            }
+        }
+        else
+        {
+            yVelocity -= gravity;
+        }
+
+        //velocity.y = yVelocity;
+        //velocity = transform.TransformDirection(velocity);
+
+        //charController.Move(movement * moveSpeed * Time.deltaTime);
+
     }
 }
