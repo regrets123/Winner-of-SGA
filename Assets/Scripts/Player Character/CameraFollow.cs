2334e12be3f51b5cefe3700c23325555c30372e8
TacoBellDoge <aforandreas@hotmail.com>
2018-02-23 12:56:32 +0100

    Trap Scripts

--- Assets/Scripts/Player Character/CameraFollow.cs
+++ Assets/Scripts/Player Character/CameraFollow.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*By Andreas Nilsson*/
 public class CameraFollow : MonoBehaviour
 {
 
---
+++ Assets/TrapDamageTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapDamageTrigger : MonoBehaviour
+{
+    [SerializeField]
+    protected int damage;
+
+    public virtual void DealDamage(IKillable target)
+    {
+        target.TakeDamage(damage);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            DealDamage(other.gameObject.GetComponent<IKillable>());
+        }
+    }
+}
