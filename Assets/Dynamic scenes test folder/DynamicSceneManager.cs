a23eaa091df9cb7ec03b9568c4ae2c85c85276fa
TacoBellDoge <aforandreas@hotmail.com>
2018-03-22 14:14:49 +0100

    Dynamic scenes created

---
+++ Assets/Dynamic scenes test folder/DynamicSceneManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DynamicSceneManager : MonoBehaviour
+{
+
+    public static DynamicSceneManager instance { get; set; }
+
+    // Use this for initialization
+    private void Awake()
+    {
+        instance = this;
+        Load("Player Scene");
+        Load("Dynamic Scene 1");
+        Load("Dynamic Scene 2");
+    }
+
+    public void Load(string sceneName)
+    {
+        if (!SceneManager.GetSceneByName(sceneName).isLoaded)
+        {
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        }
+    }
+
+    public void UnLoad(string sceneName)
+    {
+        if (SceneManager.GetSceneByName(sceneName).isLoaded)
+        {
+            SceneManager.UnloadSceneAsync(sceneName);
+        }
+    }
+}
