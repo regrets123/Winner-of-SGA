fa85cdeaa0d4bb34a4a061fec8dd88db2fdc8e93
regrets123 <lotus.saxe@gmail.com>
2018-04-10 14:47:03 +0200

    More scene fixing
    Moving player final scene contet to Father scene and fixing old assets folders.

---
+++ Assets/Scripts/MenuScripts/MenuPlayerScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPlayerScript : MonoBehaviour
+{
+    
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Animator>().SetFloat("Speed", 0f);
+    }
+
+}
--- Assets/Scripts/MenuScripts/SettingsMenuScript.cs
+++ Assets/Scripts/MenuScripts/SettingsMenuScript.cs
@@ -7,7 +7,7 @@ using System.Xml;
 using System.Xml.XPath;
 using System.IO;
 
-/*By Johanna Pettersson*/
+/*By Johanna Pettersson and Björn Andersson*/
 
 public class SettingsMenuScript : MonoBehaviour
 {
@@ -31,6 +31,11 @@ public class SettingsMenuScript : MonoBehaviour
     void Awake()
     {
         camFollow = cam.GetComponent<CameraFollow>();
+        if (sensitivitySlider == null)
+        {
+            this.sensitivitySlider = GameObject.Find("SensitivitySlider").GetComponent<Slider>();
+        }
+
         if (File.Exists(Application.dataPath + "/Settings.xml"))
         {
             settingsXML = new XmlDocument();
@@ -39,13 +44,19 @@ public class SettingsMenuScript : MonoBehaviour
             xNav = settingsXML.CreateNavigator();
             SetMusicVolume(float.Parse(xNav.SelectSingleNode("/Settings/Volumes/@Music").Value));
             startingMusic = musicVolume;
+            musicSlider.value = musicVolume;
             SetEnvironmentalVolume(float.Parse(xNav.SelectSingleNode("/Settings/Volumes/@Environmental").Value));
             startingEnvironmental = environmentalVolume;
+            environmentalSlider.value = environmentalVolume;
             SetSFXVolume(float.Parse(xNav.SelectSingleNode("/Settings/Volumes/@FX").Value));
             startingFX = SFXVolume;
+            fxSlider.value = SFXVolume;
             brightnessSlider.value = float.Parse(xNav.SelectSingleNode("/Settings/Visual/@Gamma").Value);
             startingBrightness = brightnessSlider.value;
             sensitivitySlider.value = float.Parse(xNav.SelectSingleNode("/Settings/Camera/@Sensitivity").Value);
+            camSensitivity = sensitivitySlider.value;
+            print(sensitivitySlider.value);
+            print(camSensitivity);
             startingSense = sensitivitySlider.value;
         }
         else
@@ -56,6 +67,7 @@ public class SettingsMenuScript : MonoBehaviour
             xNav = settingsXML.CreateNavigator();
         }
         RenderSettings.ambientLight = new Color(brightnessSlider.value, brightnessSlider.value, brightnessSlider.value, 1);
+        print(this.sensitivitySlider);
     }
 
     public void SetMusicVolume(float musicVolume)
@@ -70,10 +82,15 @@ public class SettingsMenuScript : MonoBehaviour
         this.environmentalVolume = environmentalVolume;
     }
 
-    public void SetCamSensitivity(float sensitivity)
+    public void SetCamSensitivity()
     {
-        camFollow.InputSensitivity = sensitivity;
-        this.camSensitivity = sensitivity;
+        if (sensitivitySlider == null)
+        {
+            sensitivitySlider = GameObject.Find("SensitivitySlider").GetComponent<Slider>();
+        }
+        camSensitivity = sensitivitySlider.value;
+        if (camFollow != null)
+            camFollow.InputSensitivity = camSensitivity;
     }
 
     public void SetSFXVolume(float SFXVolume)
@@ -100,7 +117,7 @@ public class SettingsMenuScript : MonoBehaviour
         xNav.SelectSingleNode("/Settings/Camera/@Sensitivity").SetValue(sensitivitySlider.value.ToString());
         XmlWriterSettings writerSettings = new XmlWriterSettings();
         writerSettings.Indent = true;
-        XmlWriter writer = XmlWriter.Create(Application.dataPath + "Settings.xml", writerSettings);
+        XmlWriter writer = XmlWriter.Create(Application.dataPath + "/Settings.xml", writerSettings);
         settingsXML.Save(writer);
     }
 
@@ -111,7 +128,8 @@ public class SettingsMenuScript : MonoBehaviour
         SetSFXVolume(startingFX);
         SetMusicVolume(startingMusic);
         SetEnvironmentalVolume(startingEnvironmental);
-        SetCamSensitivity(startingSense);
+        sensitivitySlider.value = startingSense;
+        SetCamSensitivity();
         settingsMenu.SetActive(false);
     }
 
