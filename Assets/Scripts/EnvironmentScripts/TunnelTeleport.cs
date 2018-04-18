22c543ff5e77d9fa4d22ded8ac9389c7fcc2087d
KazzTruul <kazztruul@hotmail.com>
2018-04-18 16:33:26 +0200

    teleport

---
+++ Assets/Scripts/EnvironmentScripts/TunnelTeleport.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*By Björn Andersson*/
+
+public class TunnelTeleport : MonoBehaviour {
+
+    [SerializeField]
+    Transform destination;
+
+    private void OnTriggerEnter(Collider other)             //Teleporterar spelaren
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.transform.position = destination.position;
+            other.gameObject.transform.rotation = destination.rotation;
+        }
+    }
+}
