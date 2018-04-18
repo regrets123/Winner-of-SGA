42dd8af8330cc23373a15105c06fae0323ad5ca2
KazzTruul <kazztruul@hotmail.com>
2018-04-18 19:14:48 +0200

    hoppas detta funkar

---
+++ Assets/Scripts/EnemyScripts/GuardianMiniBoss.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardianMiniBoss : GuardianAI {
+
+    [SerializeField]
+    GameObject myDrop;
+
+    protected override void Death()
+    {
+        base.Death();
+        Instantiate(myDrop);
+    }
+}
