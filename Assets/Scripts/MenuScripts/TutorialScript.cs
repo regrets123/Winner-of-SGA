f2294d181835c9683c0a477370c1b4bd665bf482
MythBlack <johannapettersson96@hotmail.com>
2018-04-18 16:25:45 +0200

    TutorialFininshed
    Tutorial är tillfälligt färdig.

--- Assets/Scripts/MenuScripts/TutorialScript.cs
+++ Assets/Scripts/MenuScripts/TutorialScript.cs
@@ -1,23 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+/* By Johanna Pettersson */
 
 public class TutorialScript : MonoBehaviour {
     
-    public GameObject textTutorial;
+    public Text textTutorial;
+    public string computerText;
+    public string controllerText;
 
+    // On trigger enter avslutar vi eventuellt föregående coroutine och startar den nya
     void OnTriggerEnter()
     {
+        StopCoroutine(TextCountdown());
         StartCoroutine(TextCountdown());
+        Destroy(this.gameObject, 6f);
     }
 
+    // Aktiverar texten och väntar fem sec innan den inaktiveras
     public IEnumerator TextCountdown()
-    {
-        textTutorial.SetActive(true);
-
+    { 
+        if(Input.GetJoystickNames().Length > 0)
+        {
+            textTutorial.text = controllerText;
+        }
+        else
+        {
+        textTutorial.text = computerText;
+        }
+        textTutorial.gameObject.SetActive(true);
         yield return new WaitForSeconds(5);
-
-        textTutorial.SetActive(false);
-        Destroy(this.gameObject);
+        textTutorial.gameObject.SetActive(false);
     }
 }
